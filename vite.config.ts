487199a70d46651dc27463ad5d06cbf37da3332e
Nimrod Huberman <cconnect@netvision.net.il>
2026-08-28 10:06:51 +0300

    netlify settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [
+    reactRouter(),
+    netlifyReactRouter(),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
